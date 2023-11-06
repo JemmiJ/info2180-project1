68b9d1a3e43072ab2747cbcb66129b3d653fc2bc
Jemoi Johnson <Jemoiae@gmail.com>
2023-11-05 22:17:44 -0500

    Landing page files

--- scripts.js
+++ scripts.js
@@ -1,1 +1,15 @@
 /* Add your JavaScript to this file */
+const inputtext = document.getElementById('email');
+const msg = document.getElementsByClassName('message')
+const submit = document.getElementsByClassName('btn');
+
+submit.addEventListener('click', evt=>{
+    if(inputtext){
+        msg.innerHTML = 'Thank you your email address ' + value+ ' has been added to our mailing list';
+        msg.classList.add("emailentered");
+    }
+    else{
+        msg.innerHTML = 'Please enter a valid email address';
+        msg.classList.add("noemail");
+    }
+})
